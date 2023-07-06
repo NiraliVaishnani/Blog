05b72a019ad2bfafb32864b3c1a6f713a9178cba
Nirali <nirali@evincedev.com>
2023-07-06 17:47:16 +0530

    Comment console response in setting-save

--- client/src/Components/Main/Setting/setting-save.js
+++ client/src/Components/Main/Setting/setting-save.js
@@ -12,7 +12,7 @@ const SettingForm = () => {
             fetch(`http://localhost:5000/api/setting/${id}`)
                 // .then((response) => response.json())
                 .then(response => {
-                    console.log(response);
+                    // console.log(response);
                     return response.json();
                 })
                 .then((data) => {
