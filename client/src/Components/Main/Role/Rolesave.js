a48ebe8a0c8dc3fb56bdafda322188299e238234
Nirali Vaishnani <nirali.vaishnani36@gmail.com>
2023-07-13 10:06:20 +0530

    role

--- client/src/Components/Main/Role/Rolesave.js
+++ client/src/Components/Main/Role/Rolesave.js
@@ -65,7 +65,6 @@
 //                 <input type="checkbox" value="User_list" onChange={handleChange}></input><label>User_list</label>
 //                 <button type="submit" className="add-template-button2">
 
-
 //                     {id ? 'Save Changes' : 'Submit'}
 //                 </button>
 //             </form>
@@ -75,90 +74,195 @@
 
 // export default Rolesave;
 
+// import React, { useState, useEffect } from 'react';
+// import { useParams, useNavigate } from 'react-router-dom';
 
+// const Rolesave = () => {
+//     const { id } = useParams();
+//     const navigate = useNavigate();
+//     const [roleName, setRoleName] = useState('');
+//     const [permissionName, setPermissionName] = useState('');
 
-import React, { useState, useEffect } from 'react';
-import { useParams, useNavigate } from 'react-router-dom';
+//     useEffect(() => {
+//         if (id) {
+//             fetch(`http://localhost:5000/api/userrole/${id}`)
+//                 .then((response) => response.json())
+//                 .then((data) => {
+//                     setRoleName(data.rolename);
+//                     setPermissionName(data.permissionName);
+//                 })
+//                 .catch((error) => console.log(error));
+//         }
+//     }, [id]);
+
+//     const handleSubmit = async (e) => {
+//         e.preventDefault();
+
+//         try {
+//             const url = id ? `http://localhost:5000/api/userrole/${id}` : 'http://localhost:5000/api/userrole';
+//             const response = await fetch(url, {
+//                 method: 'POST',
+//                 headers: {
+//                     'Content-Type': 'application/json',
+//                 },
+//                 body: JSON.stringify({ rolename: roleName, permissionName }),
+//             });
+
+//             const data = await response.json();
+
+//             console.log(data);
+//             navigate(`/role`);
+//         } catch (error) {
+//             console.log(error);
+//         }
+//     };
+
+//     return (
+//         <div className="role-save-container">
+//             <form onSubmit={handleSubmit}>
+//                 {id ? <h2>Edit Role</h2> : <h2>Add Role</h2>}
+//                 <label>Role Name</label>
+//                 <input type="text" value={roleName} onChange={(e) => setRoleName(e.target.value)}></input>
+//                 <label>Permission Name</label>
+
+//                 <input
+//                     type="checkbox"
+//                     checked={permissionName === 'BlogAdd'}
+//                     onChange={(e) => setPermissionName(e.target.checked ? 'BlogAdd' : '')}
+//                 />
+//                 <label>Blog Add</label>
+//                 <input
+//                     type="checkbox"
+//                     checked={permissionName === 'BlogEdit'}
+//                     onChange={(e) => setPermissionName(e.target.checked ? 'BlogEdit' : '')}
+//                 />
+//                 <label>Blog Edit</label>
+//                 <input
+//                     type="checkbox"
+//                     checked={permissionName === 'BlogDelete'}
+//                     onChange={(e) => setPermissionName(e.target.checked ? 'BlogDelete' : '')}
+//                 />
+//                 <label>Blog Delete</label>
+//                 <input
+//                     type="checkbox"
+//                     checked={permissionName === 'Userlist'}
+//                     onChange={(e) => setPermissionName(e.target.checked ? 'Userlist' : '')}
+//                 />
+//                 <label>User list</label>
+
+//                 <button type="submit" className="add-template-button2">
+//                     {id ? 'Save Changes' : 'Submit'}
+//                 </button>
+//             </form>
+//         </div>
+//     );
+// };
+
+// export default Rolesave;
+
+import React, { useState, useEffect } from "react";
+import { useParams, useNavigate } from "react-router-dom";
 
 const Rolesave = () => {
-    const { id } = useParams();
-    const navigate = useNavigate();
-    const [roleName, setRoleName] = useState('');
-    const [permissionName, setPermissionName] = useState('');
+  const { id } = useParams();
+  const navigate = useNavigate();
+  const [roleName, setRoleName] = useState("");
+  const [permissionNames, setPermissionNames] = useState([]);
 
-    useEffect(() => {
-        if (id) {
-            fetch(`http://localhost:5000/api/userrole/${id}`)
-                .then((response) => response.json())
-                .then((data) => {
-                    setRoleName(data.rolename);
-                    setPermissionName(data.permissionName);
-                })
-                .catch((error) => console.log(error));
-        }
-    }, [id]);
+  useEffect(() => {
+    if (id) {
+      fetch(`http://localhost:5000/api/userrole/${id}`)
+        .then((response) => response.json())
+        .then((data) => {
+          setRoleName(data.rolename);
+          setPermissionNames(data.permissionNames);
+        })
+        .catch((error) => console.log(error));
+    }
+  }, [id]);
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
+  const handleSubmit = async (e) => {
+    e.preventDefault();
 
-        try {
-            const url = id ? `http://localhost:5000/api/userrole/${id}` : 'http://localhost:5000/api/userrole';
-            const response = await fetch(url, {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({ rolename: roleName, permissionName }),
-            });
+    try {
+      const url = id
+        ? `http://localhost:5000/api/userrole/${id}`
+        : "http://localhost:5000/api/userrole";
+      const response = await fetch(url, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ rolename: roleName, permissionNames }),
+      });
 
-            const data = await response.json();
+      const data = await response.json();
 
-            console.log(data);
-            navigate(`/role`);
-        } catch (error) {
-            console.log(error);
-        }
-    };
+      console.log(data);
+      navigate(`/role`);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-    return (
-        <div className="role-save-container">
-            <form onSubmit={handleSubmit}>
-                {id ? <h2>Edit Role</h2> : <h2>Add Role</h2>}
-                <label>Role Name</label>
-                <input type="text" value={roleName} onChange={(e) => setRoleName(e.target.value)}></input>
-                <label>Permission Name</label>
+  const handleCheckboxChange = (permission, checked) => {
+    if (checked) {
+      // Add the permission to the array if it doesn't already exist
+      setPermissionNames((prevPermissions) => [...prevPermissions, permission]);
+    } else {
+      // Remove the permission from the array if it exists
+      setPermissionNames((prevPermissions) =>
+        prevPermissions.filter((p) => p !== permission)
+      );
+    }
+  };
 
-                <input
-                    type="checkbox"
-                    checked={permissionName === 'BlogAdd'}
-                    onChange={(e) => setPermissionName(e.target.checked ? 'BlogAdd' : '')}
-                />
-                <label>Blog Add</label>
-                <input
-                    type="checkbox"
-                    checked={permissionName === 'BlogEdit'}
-                    onChange={(e) => setPermissionName(e.target.checked ? 'BlogEdit' : '')}
-                />
-                <label>Blog Edit</label>
-                <input
-                    type="checkbox"
-                    checked={permissionName === 'BlogDelete'}
-                    onChange={(e) => setPermissionName(e.target.checked ? 'BlogDelete' : '')}
-                />
-                <label>Blog Delete</label>
-                <input
-                    type="checkbox"
-                    checked={permissionName === 'Userlist'}
-                    onChange={(e) => setPermissionName(e.target.checked ? 'Userlist' : '')}
-                />
-                <label>User list</label>
+  return (
+    <div className="role-save-container">
+      <form onSubmit={handleSubmit}>
+        {id ? <h2>Edit Role</h2> : <h2>Add Role</h2>}
+        <label>Role Name</label>
+        <input
+          type="text"
+          value={roleName}
+          onChange={(e) => setRoleName(e.target.value)}
+        ></input>
+        <label>Permission Names</label>
 
-                <button type="submit" className="add-template-button2">
-                    {id ? 'Save Changes' : 'Submit'}
-                </button>
-            </form>
-        </div>
-    );
+        <input
+          type="checkbox"
+          checked={permissionNames.includes("BlogAdd")}
+          onChange={(e) => handleCheckboxChange("BlogAdd", e.target.checked)}
+        />
+        <label>Blog Add</label>
+
+        <input
+          type="checkbox"
+          checked={permissionNames.includes("BlogEdit")}
+          onChange={(e) => handleCheckboxChange("BlogEdit", e.target.checked)}
+        />
+        <label>Blog Edit</label>
+
+        <input
+          type="checkbox"
+          checked={permissionNames.includes("BlogDelete")}
+          onChange={(e) => handleCheckboxChange("BlogDelete", e.target.checked)}
+        />
+        <label>Blog Delete</label>
+
+        <input
+          type="checkbox"
+          checked={permissionNames.includes("Userlist")}
+          onChange={(e) => handleCheckboxChange("Userlist", e.target.checked)}
+        />
+        <label>User list</label>
+
+        <button type="submit" className="add-template-button2">
+          {id ? "Save Changes" : "Submit"}
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default Rolesave;
